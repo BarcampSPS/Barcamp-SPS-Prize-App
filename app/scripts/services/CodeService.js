6b49ec473d0f1fb54b228e382b1c3482eb0bcf5e
Richard <rsiwady29@gmail.com>
2014-05-07 12:51:03 -0600

    Added Code Service

---
+++ app/scripts/services/CodeService.js
@@ -0,0 +1,13 @@
+/**
+ * Created by richardsiwady on 5/6/14.
+ */
+angular.module('barcampPrizeAppApp')
+    .factory('CodeService', [function(){
+        var factory = {};
+
+        factory.CheckCode = function(code){
+
+        }
+
+        return factory;
+    }]);
