de7c5925bc56386cc1f31341b1767271044fb253
Richard Siwady <richardsiwady@Richards-MacBook-Pro.local>
2014-05-06 23:22:43 -0600

    added RaffleUser Service, retrieving users and listing them in ui

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,21 @@
 'use strict';
 
 angular.module('barcampPrizeAppApp')
-  .controller('MainController', ['ParseService',function ($scope) {
+  .controller('MainController', ['$scope','ParseService','RaffleUsersService',function ($scope,ParseService,RaffleUsersService) {
 
+        var success = function(results) {
+            $scope.raffleUsers = results;
+            $scope.$apply();
+        };
+
+        var error = function(error) {
+            alert(error);
+        };
+
+        var init = function(){
+            var callbacks = {success: success, error: error};
+            RaffleUsersService.populateRaffleUsers(callbacks);
+        };
+
+        init();
   }]);
