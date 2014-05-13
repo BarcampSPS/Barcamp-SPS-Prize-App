0a3f0f92dbe58d8a2f79f37b08daf806aa7f866d
Richard <rsiwady29@gmail.com>
2014-05-12 22:18:38 -0600

    check codes, mark codes, adds user with valid code

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,15 @@
 'use strict';
 
 angular.module('barcampPrizeAppApp')
-  .controller('MainController', ['$scope','ParseService','RaffleUsersService',function ($scope,ParseService,RaffleUsersService) {
+  .controller('MainController', ['$scope','$modal','ParseService','RaffleUsersService','CodeService',
+        function ($scope,$modal,ParseService,RaffleUsersService,CodeService) {
+
+            $scope.alerts = [];
+
+        var init = function(){
+            var callbacks = {success: success, error: error};
+            RaffleUsersService.populateRaffleUsers(callbacks);
+        };
 
         var success = function(results) {
             $scope.raffleUsers = results;
@@ -12,9 +20,70 @@ angular.module('barcampPrizeAppApp')
             alert(error);
         };
 
-        var init = function(){
-            var callbacks = {success: success, error: error};
-            RaffleUsersService.populateRaffleUsers(callbacks);
+        var AddCodeSuccess = function(result){
+            if(result == undefined ){
+                $scope.code = '';
+                $scope.alerts.push({msg: 'Codigo Invalido!',type: 'danger'});
+                $scope.$apply();
+                return;
+            }
+
+            if(result.get('Used') == true){
+                $scope.code = '';
+                $scope.alerts.push({msg: 'Codigo ya ha sido utilizado!',type: 'danger'});
+                $scope.$apply();
+                return;
+            }
+
+            var modalInstance = $modal.open({
+                templateUrl: 'views/modalContent.html',
+                controller: function($scope,$modalInstance,RaffleUsersService,callbacks){
+
+                    $scope.callbacks = callbacks;
+                    $scope.RaffleUser = {Name: undefined, Email: undefined};
+
+                    var success = function(){
+                        RaffleUsersService.populateRaffleUsers(callbacks);
+                        $modalInstance.dismiss('cancel');
+                    };
+
+                    var error = function(){
+                        $modalInstance.dismiss('cancel');
+                    };
+
+                    $scope.guardar = function(){
+                        if($scope.RaffleUser.Name == undefined && $scope.RaffleUser.Email == undefined){
+                            return;
+                        }
+                        var callbacks = {success: success, error: error};
+                        RaffleUsersService.AddRaffleUser($scope.RaffleUser,callbacks);
+                    };
+
+                    $scope.cancelar = function(){
+                        $modalInstance.dismiss('cancel');
+                    };
+                },
+                size: 'lg',
+                resolve: {
+                    callbacks : function(){
+                        return {success: success, error: error};
+                    }
+                }
+            });
+
+            $scope.code = '';
+            result.set('Used',true);
+            CodeService.MarkCodeAsUsed(result);
+        };
+
+        var ErrorCodeSuccess = function(error){
+
+        };
+
+        $scope.AddCode = function(){
+            $scope.alerts = [];
+            var callbacks = {success : AddCodeSuccess, error: ErrorCodeSuccess};
+            CodeService.CheckCode($scope.code, callbacks);
         };
 
         init();
--- app/scripts/services/CodeService.js
+++ app/scripts/services/CodeService.js
@@ -5,8 +5,18 @@ angular.module('barcampPrizeAppApp')
     .factory('CodeService', [function(){
         var factory = {};
 
-        factory.CheckCode = function(code){
+        factory.CheckCode = function(code,callbacks){
+            var Codes = Parse.Object.extend("Codes");
+            var query = new Parse.Query(Codes);
+            query.equalTo("Code", code);
+            query.first({
+                success: callbacks.success,
+                error: callbacks.error
+            });
+        }
 
+        factory.MarkCodeAsUsed = function(Code){
+            Code.save();
         }
 
         return factory;
