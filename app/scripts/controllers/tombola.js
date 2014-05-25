784d3890cbc6d986c8687f3dfb216836844f89ab
Richard Siwady <rsiwady29@gmail.com>
2014-05-25 10:32:28 -0600

    select winner and delete from RaffleUsers class and add it to raffleWinners class

--- app/scripts/controllers/tombola.js
+++ app/scripts/controllers/tombola.js
@@ -4,6 +4,10 @@ angular.module('barcampPrizeAppApp')
   .controller('TombolaController', ['$scope','$modal','ParseService','RaffleUsersService','CodeService',
         function ($scope,$modal,ParseService,RaffleUsersService,CodeService) {
 
+            var getRandom = function(n){
+                return Math.floor(Math.random()*n);
+            };
+
             var init = function(){
                 var callbacks = {success: success, error: error};
                 RaffleUsersService.populateRaffleUsers(callbacks);
@@ -16,6 +20,46 @@ angular.module('barcampPrizeAppApp')
 
             var error = function(error) {
                 alert(error);
+            };
+
+            var raffleWinnerAdded = function(raffleWinner){
+                alert('Ganador: User: ' + raffleWinner.attributes.Name + " && Email: " + raffleWinner.attributes.Email);
+            };
+
+            var raffleWinnerError = function(error){
+                alert(error);
+            };
+
+            var raffleUserDeleted = function(user){
+                var callbacks = {success: raffleWinnerAdded, error: raffleWinnerError};
+                RaffleUsersService.AddRaffleWinner({
+                    Name: user.attributes.Name,
+                    Email : user.attributes.Email
+                }, callbacks);
+            };
+
+            var raffleUserDeletionError = function(error){
+                alert(error);
+            };
+
+            var raffleUserFound = function(raffleUserObject){
+                var callbacks = {success: raffleUserDeleted, error: raffleUserDeletionError};
+                RaffleUsersService.RemoveRaffleUser(raffleUserObject, callbacks);
+            };
+
+            var raffleUserNotFound = function(error){
+                alert(error);
+            };
+
+            $scope.escogerGanador = function(){
+
+                if($scope.raffleUsers.length == 0){
+                    return;
+                }
+
+                var randomWinner = $scope.raffleUsers[ getRandom( $scope.raffleUsers.length ) ];
+                var callbacks = {success: raffleUserFound, error:raffleUserNotFound };
+                RaffleUsersService.FindRaffleUsers(randomWinner, callbacks);
             };
 
             init();
