bc65b9ddf8f441e139f27c44047569ebc46afb16
Richard Siwady <rsiwady29@gmail.com>
2014-05-25 18:24:28 -0600

    fixed dependency issues

--- web.js
+++ web.js
@@ -1,7 +1,8 @@
 var gzippo = require('gzippo');
 var express = require('express');
+var morgan  = require('morgan');
 var app = express();
  
-app.use(express.logger('dev'));
+app.use(morgan({ format: 'dev', immediate: true }));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
